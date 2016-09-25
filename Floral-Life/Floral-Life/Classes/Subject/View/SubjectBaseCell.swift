832545d25fa7b9b41e031d89f680b186ffec2cc1
paomoliu <1772420837@qq.com>
2016-09-25 14:14:20 +0800

    创建列表和宫格集合视图的基本视图类

---
+++ Floral-Life/Floral-Life/Classes/Subject/View/SubjectBaseCell.swift
@@ -0,0 +1,126 @@
+//
+//  SubjectBaseCell.swift
+//  Floral-Life
+//
+//  Created by paomoliu on 16/9/25.
+//  Copyright © 2016年 Sunshine Girl. All rights reserved.
+//
+
+import UIKit
+
+class SubjectBaseCell: UICollectionViewCell
+{
+    var subject: Article? {
+        didSet {
+            pictrueView.sd_setImageWithURL(NSURL(string: subject!.smallIcon!))
+            titleLabel.text = subject!.title
+            descLabel.text = subject!.desc
+            readLabel.text = "\(subject!.read)"
+            appointLabel.text = "\(subject!.appoint)"
+        }
+    }
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        
+        setupUI()
+        backgroundColor = UIColor.whiteColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI()
+    {
+        // 添加子控件
+        contentView.addSubview(pictrueView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descLabel)
+        contentView.addSubview(lineView)
+        contentView.addSubview(readView)
+        contentView.addSubview(readLabel)
+        contentView.addSubview(appointView)
+        contentView.addSubview(appointLabel)
+        
+        // 关闭view的AutoresizingMask
+        pictrueView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        descLabel.translatesAutoresizingMaskIntoConstraints = false
+        lineView.translatesAutoresizingMaskIntoConstraints = false
+        readView.translatesAutoresizingMaskIntoConstraints = false
+        readLabel.translatesAutoresizingMaskIntoConstraints = false
+        appointView.translatesAutoresizingMaskIntoConstraints = false
+        appointLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    // MARK: - 懒加载
+    /// 图片
+    lazy var pictrueView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    /// 标题
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFontOfSize(12)
+        label.backgroundColor = UIColor.yellowColor()
+        label.sizeToFit()
+        
+        return label
+    }()
+    
+    /// 描述
+    lazy var descLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFontOfSize(10)
+        label.textColor = UIColor.grayColor()
+        label.backgroundColor = UIColor.yellowColor()
+        
+        return label
+    }()
+    
+    /// 分割线
+    lazy var lineView: UIImageView = {
+        let line = UIImageView(image: UIImage(named: "underLine_280x0_"))
+        
+        return line
+    }()
+    
+    /// 阅读图片
+    lazy var readView: UIImageView = {
+        let readView = UIImageView(image: UIImage(named: "ad_view_18x15_"))
+        
+        return readView
+    }()
+    
+    /// 阅读量
+    lazy var readLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFontOfSize(10)
+        label.textColor = UIColor.grayColor()
+        
+        return label
+    }()
+    
+    /// 点赞图片
+    lazy var appointView: UIImageView = {
+        let appointView = UIImageView(image: UIImage(named: "f_zan_19x17_"))
+        
+        return appointView
+    }()
+    
+    /// 点赞数
+    lazy var appointLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFontOfSize(10)
+        label.textColor = UIColor.grayColor()
+        
+        return label
+    }()
+}
--- Floral-Life/Floral-Life/Classes/Subject/View/SubjectGridCell.swift
+++ Floral-Life/Floral-Life/Classes/Subject/View/SubjectGridCell.swift
@@ -9,55 +9,16 @@
 import UIKit
 import SDWebImage
 
-class SubjectGridCell: UICollectionViewCell
+class SubjectGridCell: SubjectBaseCell
 {
-    var subject: Article? {
-        didSet {
-            pictrueView.sd_setImageWithURL(NSURL(string: subject!.smallIcon!))
-            titleLabel.text = subject!.title
-            descLabel.text = subject!.desc
-            readLabel.text = "\(subject!.read)"
-            appointLabel.text = "\(subject!.appoint)"
-        }
-    }
-    
-    override init(frame: CGRect)
+    override func setupUI()
     {
-        super.init(frame: frame)
-        
-        setupUI()
-        backgroundColor = UIColor.whiteColor()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupUI()
-    {
-        // 添加子控件
-        contentView.addSubview(pictrueView)
-        contentView.addSubview(titleLabel)
-        contentView.addSubview(descLabel)
-        contentView.addSubview(lineView)
-        contentView.addSubview(readView)
-        contentView.addSubview(readLabel)
-        contentView.addSubview(appointView)
-        contentView.addSubview(appointLabel)
-        pictrueView.image = UIImage(named: "ad_view_18x15_")
+        super.setupUI()
         
         // 布局前准备
         let dict = ["pictrueView": pictrueView, "titleLabel": titleLabel, "descLabel": descLabel,
                     "lineView": lineView, "readView": readView, "readLabel": readLabel,
                     "appointView": appointView, "appointLabel": appointLabel]
-        pictrueView.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        descLabel.translatesAutoresizingMaskIntoConstraints = false
-        lineView.translatesAutoresizingMaskIntoConstraints = false
-        readView.translatesAutoresizingMaskIntoConstraints = false
-        readLabel.translatesAutoresizingMaskIntoConstraints = false
-        appointView.translatesAutoresizingMaskIntoConstraints = false
-        appointLabel.translatesAutoresizingMaskIntoConstraints = false
         
         /// 布局子控件
         var cons = [NSLayoutConstraint]()
@@ -77,73 +38,4 @@ class SubjectGridCell: UICollectionViewCell
         
         contentView.addConstraints(cons)
     }
-    
-    // MARK: - 懒加载
-    /// 图片
-    private lazy var pictrueView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = UIViewContentMode.ScaleAspectFill
-        imageView.clipsToBounds = true
-        
-        return imageView
-    }()
-    
-    /// 标题
-    private lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFontOfSize(12)
-        label.backgroundColor = UIColor.yellowColor()
-        label.sizeToFit()
-        
-        return label
-    }()
-    
-    /// 描述
-    private lazy var descLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFontOfSize(10)
-        label.textColor = UIColor.grayColor()
-        label.backgroundColor = UIColor.yellowColor()
-        
-        return label
-    }()
-    
-    /// 分割线
-    private lazy var lineView: UIImageView = {
-        let line = UIImageView(image: UIImage(named: "underLine_280x0_"))
-        
-        return line
-    }()
-    
-    /// 阅读图片
-    private lazy var readView: UIImageView = {
-        let readView = UIImageView(image: UIImage(named: "ad_view_18x15_"))
-        
-        return readView
-    }()
-    
-    /// 阅读量
-    private lazy var readLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFontOfSize(12)
-        label.textColor = UIColor.grayColor()
-        
-        return label
-    }()
-    
-    /// 点赞图片
-    private lazy var appointView: UIImageView = {
-        let appointView = UIImageView(image: UIImage(named: "f_zan_19x17_"))
-        
-        return appointView
-    }()
-    
-    /// 点赞数
-    private lazy var appointLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFontOfSize(12)
-        label.textColor = UIColor.grayColor()
-        
-        return label
-    }()
 }
